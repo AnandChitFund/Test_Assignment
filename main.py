415b33f6a816ee83e65109cdfac41ec64e98f139
Anand_Maurya <79273275+coderanandmaurya@users.noreply.github.com>
2025-09-29 10:40:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
-# main.py
 def add_numbers(a, b):
-    """Return the sum of a and b. Implement this function."""
-    raise NotImplementedError
+    raise NotImplementedError("Implement this function.")
